feecee4b0553609c7a001a7bb0a57c660bdb80cc
Khaled Ez Zughayyar <ezzughayyar@gmail.com>
2020-03-11 00:15:01 -0400

    add terraform actions

---
+++ terraform/static-website.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "eu-west-2"
+}
+
+variable "bucket_name" {
+    description = "Bucket name"
+}
+
+resource "aws_s3_bucket" "s3_website" {
+  bucket = var.bucket_name
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}
