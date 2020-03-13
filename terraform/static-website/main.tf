996470c1b7ec12ecb13011ffeb8325da478f8d9e
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2020-03-13 05:39:38 -0400

    use cloud front for PR website (#6)

--- terraform/static-website/main.tf
+++ terraform/static-website/main.tf
@@ -16,11 +16,92 @@ variable "bucket_name" {
 
 resource "aws_s3_bucket" "s3_website" {
   bucket        = var.bucket_name
-  acl           = "public-read"
   force_destroy = true
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "Access identity between Cloudfront and S3"
+}
+
+resource "aws_s3_bucket_policy" "s3_website_policy" {
+  bucket = aws_s3_bucket.s3_website.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.s3_website.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
   }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = ["${aws_s3_bucket.s3_website.arn}"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
+  }
+}
+
+resource "aws_cloudfront_distribution" "cf_website" {
+  origin {
+    domain_name = aws_s3_bucket.s3_website.bucket_regional_domain_name
+    origin_id   = var.bucket_name
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
+  #aliases = [var.bucket_name]
+
+  default_cache_behavior {
+    allowed_methods  = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = var.bucket_name
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    App         = "net.khaledez.www"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+output "domain_name" {
+  value = aws_cloudfront_distribution.cf_website.domain_name
 }
