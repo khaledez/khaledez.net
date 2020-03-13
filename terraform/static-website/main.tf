92c0d3c6732808154a58e0857ac632f01aa7eb5c
Khaled Ez Zughayyar <ezzughayyar@gmail.com>
2020-03-13 00:00:27 -0400

    refactor

--- terraform/static-website/main.tf
+++ terraform/static-website/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "net.khaledez.terraform.bucket"
+    region = "eu-west-2"
+    key    = var.bucket_name
+  }
+}
+
 provider "aws" {
   region = "eu-west-2"
 }
