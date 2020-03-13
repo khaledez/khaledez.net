01cef8928ce83a4102f2fa891994cf69a8f1dcde
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2020-03-13 01:41:50 -0400

    add terraform actions to create a dev environment (#2)
    For every PR, create an environment to test and share with this format "pr-{PR-NO}.dev.khaledez.net"

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,6 @@ import React from "react"
 export default () => (
   <main style={{ maxWidth: "700px", margin: "4rem auto" }}>
     <h1>Khaled Ezzughayyar</h1>
-    <p>Hello, World! KZ</p>
+    <p>Hello, World!</p>
   </main>
 )
