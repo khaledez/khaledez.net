c79574884e622c1c5a981a1afdd83ddab8c6602c
Khaled Ez Zughayyar <ezzughayyar@gmail.com>
2020-03-14 01:22:58 -0400

    update to use yarn

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 
+let url = typeof(window) !== "undefined" ? window.location : "n/a"
+
 export default () => (
   <main style={{ maxWidth: "700px", margin: "4rem auto" }}>
     <h1>Khaled Ezzughayyar</h1>
-    <p>This site is under heavy construction</p>
+    <p>This site is under heavy construction. <br/>{url}</p>
   </main>
 )
