4adfe88ac7f9e1c4e37bd8b79ece248c8b2549e6
Khaled Ez Zughayyar <ezzughayyar@gmail.com>
2020-03-14 01:03:28 -0400

    update variable name

--- terraform/static-website/main.tf
+++ terraform/static-website/main.tf
@@ -10,12 +10,12 @@ provider "aws" {
   version = "~> 2.53"
 }
 
-variable "bucket_name" {
-  description = "Bucket name"
+variable "domain_name" {
+  description = "Domain name"
 }
 
 resource "aws_s3_bucket" "s3_website" {
-  bucket        = var.bucket_name
+  bucket        = var.domain_name
   force_destroy = true
 }
 
@@ -53,19 +53,19 @@ data "aws_iam_policy_document" "s3_policy" {
 resource "aws_cloudfront_distribution" "cf_website" {
   origin {
     domain_name = aws_s3_bucket.s3_website.bucket_regional_domain_name
-    origin_id   = var.bucket_name
+    origin_id   = var.domain_name
 
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
     }
   }
 
-  #aliases = [var.bucket_name]
+  #aliases = [var.domain_name]
 
   default_cache_behavior {
     allowed_methods  = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = var.bucket_name
+    target_origin_id = var.domain_name
 
     forwarded_values {
       query_string = false
