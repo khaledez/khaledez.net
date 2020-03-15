56d3dbc8afbb921449001f6e017cf0e486396d36
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2020-03-15 04:52:15 -0400

    use real domain for deployment, with https enabled (#7)

--- terraform/static-website/main.tf
+++ terraform/static-website/main.tf
@@ -10,13 +10,48 @@ provider "aws" {
   version = "~> 2.53"
 }
 
-variable "bucket_name" {
-  description = "Bucket name"
+provider "aws" {
+  alias   = "virginia"
+  region  = "us-east-1"
+  version = "~> 2.53"
+}
+
+variable "domain_name" {
+  description = "Domain name"
+}
+
+variable "dns_zone_domain" {
+  description = "DNS zone domain, must end with dot(.)"
+  default     = "khaledez.net."
+}
+
+variable "environment" {
+  description = "Evironment tag of the deployed resources"
+  default     = "dev"
+}
+
+variable "app_name" {
+  description = "Aplication which resources belongs to. (reverse-dns)"
+  default     = "net.khaledez.www"
+}
+
+variable "cache_ttl" {
+  description = "Default time to live for cache data"
+  default     = 0
+}
+
+locals {
+  common_tags = {
+    Environment = var.environment
+    App         = var.app_name
+  }
 }
 
 resource "aws_s3_bucket" "s3_website" {
-  bucket        = var.bucket_name
+  bucket        = var.domain_name
   force_destroy = true
+
+  tags = local.common_tags
 }
 
 resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
@@ -53,19 +88,20 @@ data "aws_iam_policy_document" "s3_policy" {
 resource "aws_cloudfront_distribution" "cf_website" {
   origin {
     domain_name = aws_s3_bucket.s3_website.bucket_regional_domain_name
-    origin_id   = var.bucket_name
+    origin_id   = var.domain_name
 
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
     }
   }
 
-  #aliases = [var.bucket_name]
+  aliases    = [var.domain_name]
+  depends_on = [aws_acm_certificate_validation.validate_cert]
 
   default_cache_behavior {
     allowed_methods  = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = var.bucket_name
+    target_origin_id = var.domain_name
 
     forwarded_values {
       query_string = false
@@ -77,7 +113,7 @@ resource "aws_cloudfront_distribution" "cf_website" {
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
-    default_ttl            = 3600
+    default_ttl            = var.cache_ttl
     max_ttl                = 86400
   }
 
@@ -87,19 +123,60 @@ resource "aws_cloudfront_distribution" "cf_website" {
 
   restrictions {
     geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE"]
+      restriction_type = "none"
     }
   }
 
-  tags = {
-    Environment = "dev"
-    App         = "net.khaledez.www"
-  }
+  tags = local.common_tags
 
   viewer_certificate {
     cloudfront_default_certificate = true
+    ssl_support_method             = "sni-only"
+    acm_certificate_arn            = aws_acm_certificate.domain_cert.arn
   }
+}
+
+data "aws_route53_zone" "primary" {
+  name         = var.dns_zone_domain
+  private_zone = false
+}
+
+resource "aws_route53_record" "www" {
+  zone_id         = data.aws_route53_zone.primary.zone_id
+  name            = var.domain_name
+  type            = "A"
+  allow_overwrite = true
+
+  alias {
+    name                   = aws_cloudfront_distribution.cf_website.domain_name
+    zone_id                = aws_cloudfront_distribution.cf_website.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  name    = aws_acm_certificate.domain_cert.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.domain_cert.domain_validation_options.0.resource_record_type
+  zone_id = data.aws_route53_zone.primary.id
+  records = [aws_acm_certificate.domain_cert.domain_validation_options.0.resource_record_value]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate" "domain_cert" {
+  provider          = aws.virginia
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+  tags              = local.common_tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "validate_cert" {
+  provider                = aws.virginia
+  certificate_arn         = aws_acm_certificate.domain_cert.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
 }
 
 output "domain_name" {
