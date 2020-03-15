0b2671f02308e3b09d5d0e3f279dc5fb4fb8b611
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2020-03-15 04:52:15 -0400

    use real domain for deployment, with https enabled (#7)

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import React from "react"
 export default () => (
   <main style={{ maxWidth: "700px", margin: "4rem auto" }}>
     <h1>Khaled Ezzughayyar</h1>
-    <p>This site is under heavy construction</p>
+    <p>This site is under heavy construction. <br />
+      <pre>{typeof (window) === "undefined" ? "" : window.location.href}</pre>
+    </p>
   </main>
 )
