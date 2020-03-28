98ecc34a85bd6d21af98ae09a769aefa8448709e
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2020-03-28 02:48:44 -0400

    fix certificate issue in infrastructure (#11)

--- terraform/infrastructure/cert-manager.tf
+++ terraform/infrastructure/cert-manager.tf
@@ -1,30 +1,40 @@
+locals {
+  domains_product = merge({ for domain in var.domains : domain => [domain, 0] },
+  { for tuple in chunklist(flatten([for key, val in var.domain_aliases : setproduct(val, setproduct([key], range(1, 1 + length(val)))) if length(val) > 0]), 3) : tuple[0] => slice(tuple, 1, 3) })
+
+  domain_alias_map = { for key, val in var.domain_aliases : key => concat([key], tolist(val)) }
+}
+
 data "aws_route53_zone" "primary" {
   name         = var.dns_zone_domain
   private_zone = false
 }
 
 resource "aws_route53_record" "cert_validation" {
-  for_each = toset(var.domains)
-  name     = aws_acm_certificate.domain_cert[each.value].domain_validation_options.0.resource_record_name
-  type     = aws_acm_certificate.domain_cert[each.value].domain_validation_options.0.resource_record_type
+  for_each = local.domains_product
+  name     = aws_acm_certificate.domain_cert[each.value[0]].domain_validation_options[each.value[1]].resource_record_name
+  type     = aws_acm_certificate.domain_cert[each.value[0]].domain_validation_options[each.value[1]].resource_record_type
   zone_id  = data.aws_route53_zone.primary.id
-  records  = [aws_acm_certificate.domain_cert[each.value].domain_validation_options.0.resource_record_value]
+  records  = [aws_acm_certificate.domain_cert[each.value[0]].domain_validation_options[each.value[1]].resource_record_value]
   ttl      = 60
 }
 
+resource "aws_acm_certificate_validation" "validate_cert" {
+  for_each                = toset(var.domains)
+  certificate_arn         = aws_acm_certificate.domain_cert[each.value].arn
+  validation_record_fqdns = [for domain in local.domain_alias_map[each.value] : aws_route53_record.cert_validation[domain].fqdn]
+}
+
 resource "aws_acm_certificate" "domain_cert" {
-  for_each          = toset(var.domains)
-  domain_name       = each.value
+  for_each    = toset(var.domains)
+  domain_name = each.value
+
+  subject_alternative_names = var.domain_aliases[each.value]
+
   validation_method = "DNS"
   tags              = local.common_tags
 
   lifecycle {
     create_before_destroy = true
   }
-}
-
-resource "aws_acm_certificate_validation" "validate_cert" {
-  for_each                = toset(var.domains)
-  certificate_arn         = aws_acm_certificate.domain_cert[each.value].arn
-  validation_record_fqdns = [aws_route53_record.cert_validation[each.value].fqdn]
 }
