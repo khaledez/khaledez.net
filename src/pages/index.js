ca0399dc000d45fbf3b4881ae21f1f9534958081
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2020-04-19 02:53:12 -0400

    sample

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,6 @@ import React from "react"
 export default () => (
   <main style={{ maxWidth: "700px", margin: "4rem auto" }}>
     <h1>Khaled Ezzughayyar</h1>
-    <p>This site is under heavy construction. <br />
-      <pre>{typeof (window) === "undefined" ? "ezzughayyar@gmail.com" : "ezzughayyar@gmail.com"}</pre>
-    </p>
+    <p>We will try start working on markdown</p>
   </main>
 )
