67c04bd9e6f72888eee33506ec861e6e48fd310d
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2020-05-02 19:34:20 -0400

    add blog starter

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,23 @@
 import Typography from "typography"
-import kirkhamTheme from "typography-theme-kirkham"
+import Wordpress2016 from "typography-theme-wordpress-2016"
 
-const typography = new Typography(kirkhamTheme)
+Wordpress2016.overrideThemeStyles = () => {
+  return {
+    "a.gatsby-resp-image-link": {
+      boxShadow: `none`,
+    },
+  }
+}
 
-export const { scale, rhythm, options } = typography
+delete Wordpress2016.googleFonts
+
+const typography = new Typography(Wordpress2016)
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== `production`) {
+  typography.injectStyles()
+}
+
 export default typography
+export const rhythm = typography.rhythm
+export const scale = typography.scale
