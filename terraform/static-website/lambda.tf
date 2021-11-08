0bebd34904e2d7faceb85d3829be30755a72469e
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2021-11-08 12:44:26 +0200

    delete lambda function definition

--- terraform/static-website/lambda.tf
+++ terraform/static-website/lambda.tf
@@ -1,31 +1,31 @@
-data "aws_iam_policy_document" "lambda_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
+# data "aws_iam_policy_document" "lambda_policy" {
+#   statement {
+#     actions = ["sts:AssumeRole"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com", "edgelambda.amazonaws.com"]
-    }
-  }
-}
+#     principals {
+#       type        = "Service"
+#       identifiers = ["lambda.amazonaws.com", "edgelambda.amazonaws.com"]
+#     }
+#   }
+# }
 
-resource "aws_iam_role" "lambda_router" {
-  name               = "${replace(var.domain_name, ".", "-")}-lambda-router"
-  assume_role_policy = data.aws_iam_policy_document.lambda_policy.json
-  provider           = aws.virginia
+# resource "aws_iam_role" "lambda_router" {
+#   name               = "${replace(var.domain_name, ".", "-")}-lambda-router"
+#   assume_role_policy = data.aws_iam_policy_document.lambda_policy.json
+#   provider           = aws.virginia
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
 
-resource "aws_lambda_function" "router" {
-  provider      = aws.virginia
-  function_name = "${replace(var.domain_name, ".", "-")}-static-website-router"
-  publish       = true
-  role          = aws_iam_role.lambda_router.arn
-  runtime       = "nodejs12.x"
+# resource "aws_lambda_function" "router" {
+#   provider      = aws.virginia
+#   function_name = "${replace(var.domain_name, ".", "-")}-static-website-router"
+#   publish       = true
+#   role          = aws_iam_role.lambda_router.arn
+#   runtime       = "nodejs12.x"
 
-  filename = "${path.root}/target/router.zip"
-  handler  = "router.handler"
+#   filename = "${path.root}/target/router.zip"
+#   handler  = "router.handler"
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
