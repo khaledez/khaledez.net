14d111fb62a08f0e4b739536c50801c7aeea07c9
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2022-01-15 15:32:56 +0200

    fix warning

--- terraform/static-website/website.tf
+++ terraform/static-website/website.tf
@@ -27,7 +27,7 @@ data "aws_iam_policy_document" "s3_policy" {
 
   statement {
     actions   = ["s3:ListBucket"]
-    resources = ["${aws_s3_bucket.s3_website.arn}"]
+    resources = [aws_s3_bucket.s3_website.arn]
 
     principals {
       type        = "AWS"
