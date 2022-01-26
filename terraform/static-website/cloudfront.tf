796ad492ce96065e6f87d27317b19d90cd7ef3eb
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2022-01-26 15:07:01 +0200

    re-arrange static-website for better readability

--- terraform/static-website/cloudfront.tf
+++ terraform/static-website/cloudfront.tf
@@ -1,31 +1,66 @@
-# data "aws_iam_policy_document" "lambda_policy" {
-#   statement {
-#     actions = ["sts:AssumeRole"]
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "Access identity between Cloudfront and S3"
+}
 
-#     principals {
-#       type        = "Service"
-#       identifiers = ["lambda.amazonaws.com", "edgelambda.amazonaws.com"]
-#     }
-#   }
-# }
+resource "aws_cloudfront_function" "cf_router" {
+  name    = join("-", [replace(var.domain_name, ".", "-"), "handler"])
+  runtime = "cloudfront-js-1.0"
+  comment = "router function to handle URLs"
+  publish = true
+  code    = file("${path.module}/router.js")
+}
 
-# resource "aws_iam_role" "lambda_router" {
-#   name               = "${replace(var.domain_name, ".", "-")}-lambda-router"
-#   assume_role_policy = data.aws_iam_policy_document.lambda_policy.json
-#   provider           = aws.virginia
+resource "aws_cloudfront_distribution" "cf_website" {
+  origin {
+    domain_name = aws_s3_bucket.s3_website.bucket_regional_domain_name
+    origin_id   = var.domain_name
 
-#   tags = local.common_tags
-# }
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
 
-# resource "aws_lambda_function" "router" {
-#   provider      = aws.virginia
-#   function_name = "${replace(var.domain_name, ".", "-")}-static-website-router"
-#   publish       = true
-#   role          = aws_iam_role.lambda_router.arn
-#   runtime       = "nodejs12.x"
+  aliases = local.aliases
 
-#   filename = "${path.root}/target/router.zip"
-#   handler  = "router.handler"
+  default_cache_behavior {
+    allowed_methods  = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = var.domain_name
 
-#   tags = local.common_tags
-# }
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.cf_router.arn
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = var.cache_ttl
+    max_ttl                = 86400
+    compress               = true
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = local.common_tags
+
+  viewer_certificate {
+    ssl_support_method  = "sni-only"
+    acm_certificate_arn = data.aws_acm_certificate.domain_cert.arn
+  }
+}
