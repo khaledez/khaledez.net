7af0d953728a05bfba9d7fd5ea79d78ba2313b1c
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2022-10-17 13:47:40 +0300

    disable s3

--- terraform/static-website/s3.tf
+++ terraform/static-website/s3.tf
@@ -1,33 +1,33 @@
-resource "aws_s3_bucket" "s3_website" {
-  bucket        = var.domain_name
-  force_destroy = true
+# resource "aws_s3_bucket" "s3_website" {
+#   bucket        = var.domain_name
+#   force_destroy = true
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
 
-resource "aws_s3_bucket_policy" "s3_website_policy" {
-  bucket = aws_s3_bucket.s3_website.id
-  policy = data.aws_iam_policy_document.s3_policy.json
-}
+# resource "aws_s3_bucket_policy" "s3_website_policy" {
+#   bucket = aws_s3_bucket.s3_website.id
+#   policy = data.aws_iam_policy_document.s3_policy.json
+# }
 
-data "aws_iam_policy_document" "s3_policy" {
-  statement {
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.s3_website.arn}/*"]
+# data "aws_iam_policy_document" "s3_policy" {
+#   statement {
+#     actions   = ["s3:GetObject"]
+#     resources = ["${aws_s3_bucket.s3_website.arn}/*"]
 
-    principals {
-      type        = "AWS"
-      identifiers = [module.tf_next.api_endpoint_access_policy_arn]
-    }
-  }
+#     principals {
+#       type        = "AWS"
+#       identifiers = [module.tf_next.api_endpoint_access_policy_arn]
+#     }
+#   }
 
-  statement {
-    actions   = ["s3:ListBucket"]
-    resources = [aws_s3_bucket.s3_website.arn]
+#   statement {
+#     actions   = ["s3:ListBucket"]
+#     resources = [aws_s3_bucket.s3_website.arn]
 
-    principals {
-      type        = "AWS"
-      identifiers = [module.tf_next.api_endpoint_access_policy_arn]
-    }
-  }
-}
+#     principals {
+#       type        = "AWS"
+#       identifiers = [module.tf_next.api_endpoint_access_policy_arn]
+#     }
+#   }
+# }
